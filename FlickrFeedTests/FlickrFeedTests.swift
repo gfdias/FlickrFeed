1a1eee151143b3dad106b7444f8e0fb01acd09a6
Gonçalo Dias <goncalo.dias@7egend.cr>
2018-06-01 14:17:22 +0100

    Initial Commit

---
+++ FlickrFeedTests/FlickrFeedTests.swift
@@ -0,0 +1,36 @@
+//
+//  FlickrFeedTests.swift
+//  FlickrFeedTests
+//
+//  Created by Gonçalo Dias on 01/06/2018.
+//  Copyright © 2018 Gonçalo Dias. All rights reserved.
+//
+
+import XCTest
+@testable import FlickrFeed
+
+class FlickrFeedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
