9e7decf80192b5ed3608f36751876be51ae5a753
Gonçalo Dias <goncalo.dias@7egend.cr>
2018-06-02 10:54:51 +0100

    Adds FlickrPhoto model

---
+++ FlickrFeed/Models/FlickrPhoto.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+struct FlickrPhoto {
+    
+    let id : String
+    let title: String
+    let description: String
+    let location: String
+    let date: Date
+        
+}
