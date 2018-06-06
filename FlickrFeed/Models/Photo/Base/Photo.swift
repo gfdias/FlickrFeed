75e9500f9422faf6c5a77977c277588f93160358
Gonçalo Dias <goncalo.dias@7egend.cr>
2018-06-06 21:12:30 +0100

    Adds tags and owner to Photo models

--- FlickrFeed/Models/Photo/Base/Photo.swift
+++ FlickrFeed/Models/Photo/Base/Photo.swift
@@ -5,6 +5,8 @@ protocol Photo {
     
     var title: String { get set }
     var date: String { get set }
+    var owner: String { get set }
+    var tags: [String] { get set }
     var url: URL { get }
     
 }
--- FlickrFeed/Models/Photo/Flickr/FlickrPhoto.swift
+++ FlickrFeed/Models/Photo/Flickr/FlickrPhoto.swift
@@ -1,13 +1,15 @@
 import Foundation
 
 struct FlickrPhoto: Photo {
-            
+   
     var title: String
     var date: String
+    var owner: String
     var photoID : String
     var farm: Int
     var server: String
     var secret: String
+    var tags: [String]
     
     var url: URL {
         return URL(string: "https://farm\(farm).staticflickr.com/\(server)/\(photoID)_\(secret)_m.jpg")!
@@ -21,10 +23,12 @@ extension FlickrPhoto: Decodable {
         
         case title = "title"
         case photoID = "id"
+        case owner = "ownername"
         case secret = "secret"
         case server = "server"
         case farm = "farm"
         case date = "datetaken"
+        case tags = "tags"
         
     }
     
@@ -34,13 +38,16 @@ extension FlickrPhoto: Decodable {
         
         let title: String = try container.decode(String.self, forKey: .title)
         let id: String = try container.decode(String.self, forKey: .photoID)
+        let owner: String = try container.decode(String.self, forKey: .owner)
+        let tags: [String] = try container.decode(String.self, forKey: .tags).components(separatedBy: " ")
         let secret: String = try container.decode(String.self, forKey: .secret)
         let server: String = try container.decode(String.self, forKey: .server)
         let farm: Int = try container.decode(Int.self, forKey: .farm)
         let date: String = try container.decode(String.self, forKey: .date)
 
-        self.init(title: title, date: date, photoID: id, farm: farm, server: server, secret: secret)
+        self.init(title: title, date: date, owner: owner, photoID: id, farm: farm, server: server, secret: secret, tags: tags)
     
     }
+    
 }
 
